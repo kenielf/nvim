37d54d7d3768a3b88b1f14e2af783efb493dab32
kenielf <chrystian.keni.github@gmail.com>
2024-08-21 16:35:04 -0300

    Added filtering rules to window-picker

--- lua/custom/ui/window-picker.lua
+++ lua/custom/ui/window-picker.lua
@@ -1,3 +1,9 @@
 require 'window-picker'.setup({
-    hint = "floating-big-letter"
+    hint = "floating-big-letter",
+    filter_rules = {
+        bo = {
+            filetype = { "neo-tree", "neo-tree-popup", "notify" },
+            buftype = { 'terminal', "quickfix" },
+        },
+    },
 })
