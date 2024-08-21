0e302cb67a59dd963f3c74582ffdd415e55e4e59
kenielf <chrystian.keni.github@gmail.com>
2024-08-21 08:08:03 -0300

    Moved ui mode manager earlier in the load order.

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 -- Enable experimental module loader
 vim.loader.enable()
 
--- Load plugin manager
-require("custom.plugins")
-
 -- Load UI Modes
 require("custom.modes")
+
+-- Load plugin manager
+require("custom.plugins")
 
 -- Load UI Theme
 require("custom.ui.theme.catppuccin")
