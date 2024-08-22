87cf9d5615fae158875bab42c3b44fba3856e37c
kenielf <chrystian.keni.github@gmail.com>
2024-08-22 12:55:10 -0300

    Removed images on neovide.

--- lua/custom/ui/images.lua
+++ lua/custom/ui/images.lua
@@ -6,11 +6,16 @@ else
     -- Make sure that ueberzug is installed if terminal is not kitty
     local ueberzug = vim.fn.exepath("ueberzug")
     if ueberzug == "" then
-        os.exit(1)
+        return
     end
 
     -- Set "ueberzug" as backend
     backend = "ueberzug"
+end
+
+-- Make sure that this is a terminal
+if not vim.g.tty and vim.g.neovide then
+    return
 end
 
 -- Configure image previews
