5e34f745024941e3b4ed4197d0da876f2beae25d
kenielf <chrystian.keni.github@gmail.com>
2024-08-22 14:54:47 -0300

    Moved markers to before plugins - to avoid issues with buffers.

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ require("custom.behavior.mouse")
 require("custom.behavior.clipboard")
 require("custom.behavior.indentation")
 require("custom.behavior.folding")
+require("custom.ui.visual-markers")
 
 -- Load quick keymaps
 require("custom.keymaps.quick-keys")
