51d87897ff9ba3f5481d6c50dec84334631ef467
kenielf <chrystian.keni.github@gmail.com>
2024-08-22 12:46:35 -0300

    Only set the visual markers on valid filetypes.

--- lua/custom/ui/visual-markers.lua
+++ lua/custom/ui/visual-markers.lua
@@ -1,12 +1,20 @@
--- Line numbering
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- Enable listchars only on valid buffers
+vim.api.nvim_create_autocmd('BufEnter', { callback = function()
+    if vim.bo.filetype ~= "dashboard" and vim.bo.filetype ~= "neo-tree" then
+        -- Line numbering
+        vim.opt.number = true
+        vim.opt.relativenumber = true
 
--- Cursor column
-vim.opt.colorcolumn = "80"
+        -- Cursor column
+        vim.opt.colorcolumn = "80"
 
--- Cursor line
-vim.opt.cursorline = true
+        -- Cursor line
+        vim.opt.cursorline = true
 
--- Indent markers
--- Character markers
+        -- Character markers
+        vim.opt.list = true
+        vim.opt.listchars:append "space:⋅"
+        vim.opt.listchars:append "eol:↴"
+    end
+end })
+
