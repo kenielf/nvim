f2f2fddd8c2c9f063c98c85c512fe7566bfb6da8
kenielf <chrystian.keni.github@gmail.com>
2024-08-24 01:17:30 -0300

    Changed keymaps to fit lhs tags.

--- lua/custom/keymaps/quick-keys.lua
+++ lua/custom/keymaps/quick-keys.lua
@@ -2,13 +2,13 @@
 vim.g.mapleader = " "
 
 -- Quick exit
-vim.keymap.set("n", "<leader>Q", "<cmd>qa!<cr>", { desc = "Quick Exit", silent = true })
+vim.keymap.set("n", "<leader><s-q>", "<cmd>qa!<cr>", { desc = "Quick Exit", silent = true })
 
 -- Quick save
-vim.keymap.set("n", "<leader>W", "<cmd>w<cr>", { desc = "Quick Save", silent = true })
+vim.keymap.set("n", "<leader><s-w>", "<cmd>w<cr>", { desc = "Quick Save", silent = true })
 
 -- Buffer navigation
-vim.keymap.set("n", "<C-Left>", "<Cmd>:bp<CR>", { desc = "Previous Buffer", noremap = true, silent = true })
-vim.keymap.set("n", "<C-Right>", "<Cmd>:bn<CR>", { desc = "Next Buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<c-left>", "<cmd>:bp<cr>", { desc = "Previous Buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<c-right>", "<cmd>:bn<cr>", { desc = "Next Buffer", noremap = true, silent = true })
 local delete = require("custom.extra.buffers").smart_bufdelete
-vim.keymap.set("n", "<C-Up>", delete, { desc = "Close Buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<c-up>", delete, { desc = "Close Buffer", noremap = true, silent = true })
