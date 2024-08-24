e0d0e5c5cabb2b520896c1f661698f409fecc8ca
kenielf <chrystian.keni.github@gmail.com>
2024-08-23 23:53:43 -0300

    Improved scrolling keymaps with descriptions and tweaked key syntax.

--- lua/custom/navigation/scrolling.lua
+++ lua/custom/navigation/scrolling.lua
@@ -2,14 +2,14 @@ local ns = require('neoscroll')
 ns.setup({ mappings = {}, })
 
 local keymap = {
-    ["<C-u>"] = function() ns.ctrl_u({ duration = 250 }) end;
-    ["<C-d>"] = function() ns.ctrl_d({ duration = 250 }) end;
-    ["zt"]    = function() ns.zt({ half_win_duration = 250 }) end;
-    ["zz"]    = function() ns.zz({ half_win_duration = 250 }) end;
-    ["zb"]    = function() ns.zb({ half_win_duration = 250 }) end;
+    ["<c-u>"] = { callback = function() ns.ctrl_u({ duration = 250 }) end, desc = "Scroll up" };
+    ["<c-d>"] = { callback = function() ns.ctrl_d({ duration = 250 }) end, desc = "Scroll down" };
+    ["zt"]    = { callback = function() ns.zt({ half_win_duration = 250 }) end, desc = "Top current line" };
+    ["zz"]    = { callback = function() ns.zz({ half_win_duration = 250 }) end, desc = "Center current line" };
+    ["zb"]    = { callback = function() ns.zb({ half_win_duration = 250 }) end, desc = "Bottom current line" };
 }
 local modes = { 'n', 'v', 'x' }
 for key, func in pairs(keymap) do
-    vim.keymap.set(modes, key, func)
+    vim.keymap.set(modes, key, func.callback, { desc = func.desc, silent = true })
 end
 
