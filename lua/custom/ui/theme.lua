0ce0bf1397d4ced941f2a4542649654d015b6fc0
kenielf <chrystian.keni.github@gmail.com>
2024-08-25 04:18:58 -0300

    Cleaned up the theme file as well.

--- lua/custom/ui/theme.lua
+++ lua/custom/ui/theme.lua
@@ -6,54 +6,57 @@ else
     if vim.g.neovide then
         transparency = false
     end
+
+    -- Theme Integrations
+    local integrations = {
+        barbecue = {
+            dim_dirname = true, -- directory name is dimmed by default
+            bold_basename = true,
+            dim_context = true,
+            alt_background = false,
+        },
+        fidget = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = { enabled = true, lsp = false },
+        indent_blankline = { enabled = true, colored_indent_levels = false, },
+        lsp_trouble = true,
+        mason = true,
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+                ok = { "italic" },
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" },
+                ok = { "underline" },
+            },
+            inlay_hints = { background = true, },
+        },
+        neogit = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        nvim_surround = true,
+        render_markdown = true,
+        telescope = { enabled = true, style = "nvchad", },
+        which_key = true,
+        window_picker = true,
+    }
+
+    -- Configuring the theme
     require("catppuccin").setup({
         flavour = "mocha",
         show_end_of_buffer = true,
         transparent_background = transparency,
-        integrations = {
-            native_lsp = {
-                enabled = true,
-                virtual_text = {
-                    errors = { "italic" },
-                    hints = { "italic" },
-                    warnings = { "italic" },
-                    information = { "italic" },
-                    ok = { "italic" },
-                },
-                underlines = {
-                    errors = { "underline" },
-                    hints = { "underline" },
-                    warnings = { "underline" },
-                    information = { "underline" },
-                    ok = { "underline" },
-                },
-                inlay_hints = {
-                    background = true,
-                },
-            },
-            barbecue = {
-                dim_dirname = true, -- directory name is dimmed by default
-                bold_basename = true,
-                dim_context = true,
-                alt_background = false,
-            },
-            flash = true,
-            gitsigns = true,
-            neogit = true,
-            nvim_surround = true,
-            render_markdown = true,
-            lsp_trouble = true,
-            which_key = true,
-            mason = true,
-            notify = true,
-            noice = true,
-            fidget = true,
-            neotree = true,
-            window_picker = true,
-            illuminate = { enabled = true, lsp = false },
-            indent_blankline = { enabled = true, colored_indent_levels = false, },
-            telescope = { enabled = true, style = "nvchad", },
-        },
+        integrations = integrations,
     })
     vim.cmd.colorscheme({ args = { "catppuccin" } })
 end
