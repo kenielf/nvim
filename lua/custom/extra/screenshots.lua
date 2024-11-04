fa3fbf0da85a81c7a410c162475a7cb9bf2f42b1
kenielf <chrystian.keni.github@gmail.com>
2024-11-04 15:13:07 -0300

    Added a secondary colorscheme for quick usage, currently temporary until I update my plugin to allow for multiple themes.

--- lua/custom/extra/screenshots.lua
+++ lua/custom/extra/screenshots.lua
@@ -22,3 +22,25 @@ vim.keymap.set(
     "v", "<leader>sc", silicon.screenshot,
     { desc = "Take a screenshot of the code", silent = true }
 )
+
+-- local alt_options = {
+--     font = "Iosevka Term=24;Noto Color Emoji=24",
+--     theme = "GitHub",
+--     background = "#FFFFFF00",
+--     pad_horiz = 40,
+--     pad_vert = 40,
+--     shadow_blur_radius = 0,
+--     shadow_offset_x = 0,
+--     shadow_offset_y = 0,
+--     no_round_corner = true,
+--     no_window_controls = true,
+--     to_clipboard = true,
+-- }
+--
+-- local alt_silicon = require("silicon")
+-- alt_silicon.setup(alt_options)
+--
+-- vim.keymap.set(
+--     "v", "<leader>sC", alt_silicon.screenshot,
+--     { desc = "Take a screenshot of the code using the alternative scheme", silent = true }
+-- )
