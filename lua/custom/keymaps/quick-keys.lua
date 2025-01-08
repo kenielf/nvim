16c9b2001aca4245a7425fd5efd05ee6d26e2dd9
kenielf <chrystian.keni.github@gmail.com>
2025-01-08 05:36:05 -0300

    Added quick sort options

--- lua/custom/keymaps/quick-keys.lua
+++ lua/custom/keymaps/quick-keys.lua
@@ -7,8 +7,12 @@ vim.keymap.set("n", "<leader><s-q>", "<cmd>qa!<cr>", { desc = "Quick Exit", sile
 -- Quick save
 vim.keymap.set("n", "<leader><s-w>", "<cmd>w<cr>", { desc = "Quick Save", silent = true })
 
+-- Quick sort
+vim.keymap.set("n", "<leader>ss", "<cmd>sort<cr>", { desc = "Quick sort", silent = true })
+vim.keymap.set("n", "<leader>s<s-s>", "<cmd>sort!<cr>", { desc = "Quick sort (reverse)", silent = true })
+
 -- Buffer navigation
-vim.keymap.set("n", "<c-left>", "<cmd>:bp<cr>", { desc = "Previous Buffer", noremap = true, silent = true })
-vim.keymap.set("n", "<c-right>", "<cmd>:bn<cr>", { desc = "Next Buffer", noremap = true, silent = true })
+vim.keymap.set({"n", "v"}, "<c-left>", "<cmd>:bp<cr>", { desc = "Previous Buffer", noremap = true, silent = true })
+vim.keymap.set({"n", "v"}, "<c-right>", "<cmd>:bn<cr>", { desc = "Next Buffer", noremap = true, silent = true })
 local delete = require("custom.extra.buffers").smart_bufdelete
 vim.keymap.set("n", "<c-up>", delete, { desc = "Close Buffer", noremap = true, silent = true })
