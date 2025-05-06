f0db7df4ef52f6675572aecb40195ef9d1bfc363
kenielf <chrystian.keni.github@gmail.com>
2025-05-06 03:12:51 -0300

    feat(folding)!: force treesitter fold recompute if available

--- lua/custom/behavior/folding.lua
+++ lua/custom/behavior/folding.lua
@@ -2,3 +2,25 @@
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
+
+-- Update folds on read
+vim.api.nvim_create_autocmd("User", {
+    pattern = "TreesitterFold",  -- Custom event defined below
+    callback = function() vim.cmd("normal! zx|zR") end
+})
+
+local compute_folds = function(args)
+    local tsp = require("nvim-treesitter.parsers")
+    local buf = args.buf
+    local lang = tsp.get_buf_lang(buf)
+    if tsp.has_parser(lang) then
+        vim.defer_fn(function()
+            vim.api.nvim_exec_autocmds("User", { pattern = "TreesitterFold" })
+        end, 50)
+    end
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = compute_folds
+})
+
